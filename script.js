ed7d76a70385016e48f0bd4ec5ef77d9b9b88ee7
jeevakavie-ux <jeevakavie@gmail.com>
2026-06-15 21:37:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(amount) {
+	let q = Math.floor(amount / 25);
+	amount %= 25;
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+	let d = Math.floor(amount / 10);
+	amount %= 10;
+
+	let n = Math.floor(amount / 5);
+	amount %= 5;
+
+	let p = amount;
+
+	return {
+		 q: q,
+		 d: d,
+		 n: n;
+		 p: p
+	};
+}
+
+
+
+
